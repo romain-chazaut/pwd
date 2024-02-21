41cbf309257f4e92b2f74c369502cbd782020e55
romain-chazaut <romain.chazaut@laplateforme.io>
2024-02-21 10:25:07 +0100

    wip

--- index.php
+++ index.php
@@ -1,49 +1,38 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+$router = new AltoRouter();
+
+$router->setBasePath('/pwd');
+
+$router->map('GET', '/', function() {
+    echo "Hello homepage";
+});
+
+// Route de la page de connexion
+$router->map('GET', '/login', function() {
+    // Assurez-vous que le chemin vers votre fichier de vue est correct.
+    require __DIR__ . '/View/login.php';
+});
+
+// Route de la liste des produits
+$router->map('GET', 'View/product', function() {
+    echo "Hello products list";
+});
+
+// Route d'un produit spécifique par ID
+$router->map('GET', 'View/product/[i:id]', function($id) {
+    echo "Hello product with id : $id";
+});
+
+// Correspondance de la route actuelle et exécution du handler correspondant
+$match = $router->match();
+
+if(is_array($match) && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']); 
+} else {
+    // Si aucune route ne correspond, renvoyer une erreur 404
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    echo "404 Not Found";
 }
-
-if (isset($_SESSION['user'])) {
-    $user = $_SESSION['user'];
-}
-?>
-
-<!doctype html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="icon" href="./public/assets/img/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="./public/assets/css/style.css">
-        <title>Bienvenue</title>
-    </head>
-
-    <body>
-        <h1>Bienvenue</h1>
-        <div class="welcome-button">
-          <button class="shop-button">
-            <a href="View/shop.php">Shop</a>
-        </button>
-
-        <button class="profile-button">
-            <a href="View/profile.php">Profile</a>
-        </button>
-
-        <button class="logout-button">
-            <a href="View/logout.php">Logout</a>
-        </button>  
-        </div>
-        
-
-        <?php if (isset($_SESSION['success'])) { ?>
-            <p><?= $_SESSION['success']?></p>
-            <?php unset($_SESSION['success']); ?>
-        <?php } ?>
-    </body>
-</html>
